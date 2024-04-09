1e51831ff9fcba25722fe95a13bd8bad9de35135
Bhumika gupta <bg0674079@gmail.com>
2024-04-09 18:53:15 +0530

    weather

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+export default App
